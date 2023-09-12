5dc1fae7e133d73dcfd8f1a0fe65f845d42ba631
Bonsbilibons <bonsbilibons@gmail.com>
2023-09-12 15:12:55 +0300

    first commit

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     unique_id = models.CharField("unique_id" , max_length=100)
+    email = models.EmailField("email", unique=True)
     class Meta:
         db_table = 'auth_user'
